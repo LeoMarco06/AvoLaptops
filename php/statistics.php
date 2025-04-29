a6b74127d3377a1f2119d8cf3c52e590f8e1f9f7
Leonardo Marco Campli <camplifamily@gmail.com>
2025-04-29 21:10:05 +0200

    feat: Add statistics page with layout and styles, including new CSS for statistics display and some css improvements.

---
+++ php/statistics.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="it" data-theme="light">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+    <!-- Title of the browser tab -->
+    <title>Avo Laptops | Statistiche</title>
+
+    <!-- Link to the styles sheet -->
+    <link rel="stylesheet" href="../css/styles.css">
+
+    <!-- Link to the responsive styles sheet -->
+    <link rel="stylesheet" href="../css/responsive.css">
+    
+    <!-- Link to the statistics styles sheet -->
+    <link rel="stylesheet" href="../css/statistics.css">
+
+    <!-- Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+    <!-- Script that manages the theme mode, animations, navbar... -->
+    <script src="../js/page_setup.js" defer></script>
+</head>
+
+<body id="home">
+    <?php include_once 'header_navbar.php'; ?>
+
+    <main>
+        <section class="main-banner">
+            <div class="main-container">
+                <div class="banner-content">
+                    <h1 class="heading-1">Statistiche</h1>
+                    <p>In questa pagina puoi trovare infografiche e dati sulle prenotazioni che sono state fatte fino al
+                        giorno d'oggi, ad esempio in che orari sono più richiesti, per quante ore in media, se sono
+                        richiesti portatili maggiormente da
+                        professori o professoresse e così via.</p>
+                </div>
+            </div>
+        </section>
+
+        <section id="servizi" class="features-section">
+            <div class="main-container">
+                <div class="statistics-container">
+                    <div class="statistic-item">
+                        <h2 class="header2">PERCENTUALI MASCHI E FEMMINE</h2>
+                    </div>
+                    <div class="statistic-item">
+                        <h2 class="header2">PERCENTUALI MASCHI E FEMMINE</h2>
+                    </div>
+                    <div class="statistic-item">
+                        <h2 class="header2">PERCENTUALI MASCHI E FEMMINE</h2>
+                    </div>
+                    <div class="statistic-item">
+                        <h2 class="header2">PERCENTUALI MASCHI E FEMMINE</h2>
+                    </div>
+                    <div class="statistic-item">
+                        <h2 class="header2">PERCENTUALI MASCHI E FEMMINE</h2>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+
+    <?php include_once "footer.php" ?>
+
+</body>
+
+</html>
